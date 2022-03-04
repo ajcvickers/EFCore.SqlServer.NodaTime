8bbf4a72c8c7428462644f3d0c0277ac17e5fb84
Steven Rasmussen <steven.rasmussen@gmail.com>
2022-03-03 20:30:33 -0700

    Added a new unit test for debugging #27

---
+++ src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Tests/ServiceCollectionTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Tests.Models;
+using Xunit;
+
+namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Tests
+{
+    public class ServiceCollectionTest
+    {
+        [Fact]
+        public void CustomServiceCollection_ShouldNotThrowError()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkSqlServer()
+                .AddNodaTime()
+                .BuildServiceProvider(true);
+
+            var builder = new DbContextOptionsBuilder<RacingContext>()
+                .UseInternalServiceProvider(serviceProvider)
+                .UseSqlServer("Does not matter since we won't try to connect to the DB", opt =>
+                {
+                    opt.UseNodaTime();
+                });
+
+            var dbContext = new RacingContext(builder.Options);
+        }
+    }
+}
